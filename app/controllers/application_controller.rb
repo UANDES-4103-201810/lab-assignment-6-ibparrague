71faa2deca6485b6959c6e6ce495c308e84ed5a5
Ignacio Parragué <ibparrague@gmail.com>
2018-04-17 20:50:45 -0300

    sign in partially done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,14 +7,15 @@ class ApplicationController < ActionController::Base
 
   def current_user
 	#complete this method
-	@_current_user ||= session[:current_user_id] && User.find_by(id: session[:current_user_id])
-	
+	@_current_user ||= session[:current_user_id] &&
+		User.find_by(id: session[:current_user_id])
 	
   end
 
   def is_user_logged_in?
 	#complete this method
   	logged_in = false
+	logged_in = session[:current_user_id].present?
 	if logged_in then true else redirect_to root_path end 
 	
   end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,14 +4,25 @@ class SessionsController < ApplicationController
 
 	def create
 		#complete this method
-		
-		if is_user_logged_in? == true
-			redirect_to root_url,notice: "OK"
+		user = User.find_by(email: params[:session][:email])
+	#	password = params[:session][:password]
+	#	id = params[:session][:id]
+		confirm = user.nil?
+		ok = false
+		if confirm != true
+			ok = true
+		end
+		if ok && user.email == params[:session][:email] && user.password == params[:session][:password]
+			flash[:notice]="You have successfully logged in"
+			redirect_to user_path(user.id)
+		else
+			flash[:notice] = "Error."
+			redirect_to root_url
 		end
 	end
 
 	def destroy
 		#complete this method
-
+		
 	end
 end
